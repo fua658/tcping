ac20de3013285cfde3442c6a2d87b1e3cf522364
fua658 <fua658@foxmail.com>
2024-05-09 17:25:46 +0800

    Update tcp.h

--- tcp.h
+++ tcp.h
@@ -1,3 +1,4 @@
+//tcp.h
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -6,5 +7,7 @@
 #ifndef TCP_H_DEFINED
 #define TCP_H_DEFINED
 int lookup(char *host, char *portnr, struct addrinfo **res);
-int connect_to(struct addrinfo *addr, struct timeval *rtt);
+
+int connect_to(struct addrinfo *addr, struct timeval *rtt, int timeout);
+
 #endif /* TCP_H_DEFINED */
